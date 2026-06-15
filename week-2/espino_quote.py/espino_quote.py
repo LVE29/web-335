acd8babe82caf9d7881a37fbfdecab1628df3f78
Leslie Espino <via.esp56@gmail.com>
2026-06-14 21:37:20 -0500

    Added Exercise 2.3 Python quote program

---
+++ week-2/espino_quote.py/espino_quote.py
@@ -0,0 +1,10 @@
+"""
+Author: Leslie Espino
+Date: June 14, 2026
+File Name: espino_quote.py
+Description: This program prints a quote of my choosing and credits the author.
+"""
+
+# Print a quote and the person credited for the quote
+print("There is no recipe, there is no one way to do things — there is only your way. And if you can recognize that in yourself and accept and appreciate that in others, you can make magic.")
+print("- Ara Katz, Chief Marketing Officer, Spring")
